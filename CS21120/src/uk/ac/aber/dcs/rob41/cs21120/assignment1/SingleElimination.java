2e19f39798be39c6cd35c83c15706c4a12ed51b8
Rob Bayliss <rob41@aber.ac.uk>
2014-11-19 13:10:37 +0000

    Update SingleElimination.java

--- CS21120/src/uk/ac/aber/dcs/rob41/cs21120/assignment1/SingleElimination.java
+++ CS21120/src/uk/ac/aber/dcs/rob41/cs21120/assignment1/SingleElimination.java
@@ -7,28 +7,32 @@ import uk.ac.aber.dcs.bpt.cs21120.assignment1.Match;
 import uk.ac.aber.dcs.bpt.cs21120.assignment1.NoNextMatchException;
 
 public class SingleElimination implements IManager {
-	ArrayList<String> competitors;
-	Boolean Final = false;
-	/**
+    Queue competitors = new Queue();
+    String currentMatchPlayer1;
+    String currentMatchPlayer2;
+    Boolean Final = false;
+    /**
      * Set the players or teams to use in the competition
      * @param players the players or teams
      */
     public void setPlayers(ArrayList<String> players) {
-    	competitors = new ArrayList<String> (players);
-	}
+        for (int counter = 0; counter<players.size(); counter++){
+            competitors.enQ(players.get(counter));
+        }      
+    }
    
     /**
      * Return true if there is another match in the competition that can be fetched using nextMatch
      * @return returns true if the competition is still going
      */
     public boolean hasNextMatch() {
-    	
-    	if(competitors.size()<=1 ||  Final == true){
-    		return false;	
-    	}else{
-    		return true;
-    	} 		
-	}
+        
+        if(competitors.length()<=1 ||  Final == true){
+            return false;    
+        }else{
+            return true;
+        }         
+    }
     
     /**
      * Returns the nextMatch to play
@@ -36,45 +40,31 @@ public class SingleElimination implements IManager {
      * @throws NoNextMatchException if the competition is over and no more matches
      */
     public Match nextMatch() throws NoNextMatchException {
-    	Match match = new Match((competitors.get(0)),(competitors.get(1)));
-    	
-		return match;
-	}
+        currentMatchPlayer1 = competitors.deQ().toString();
+        currentMatchPlayer2 = competitors.deQ().toString();
+        Match match = new Match(currentMatchPlayer1, currentMatchPlayer2);
+        
+        return match;
+    }
     
     /** Sets the winner for the last retrieved Match
      * 
      * @param player1 should be true if player1 won the match, otherwise false
      */
     public void setMatchWinner(boolean player1) {
-    	if (competitors.size() == 2){
-    		Final = true;
-	    	 if (player1 == true){
-	    		String winner = competitors.get(0);
-	    		String runnerUp = competitors.get(1);
-	    		competitors.add(winner);
-	    		competitors.add(runnerUp);
-	    		competitors.remove(1);
-	    		competitors.remove(0);
-	    	}else{
-	    		String winner = competitors.get(1);
-	    		String runnerUp = competitors.get(0);
-	    		competitors.add(winner);
-	    		competitors.add(runnerUp);
-	    		competitors.remove(1);
-	    		competitors.remove(0);
-	    	}
-    	}else if (player1 == true){
-    		String winner = competitors.get(0);
-    		competitors.add(winner);
-    		competitors.remove(1);
-    		competitors.remove(0);
-    	}else{
-    		String winner = competitors.get(1);
-    		competitors.add(winner);
-    		competitors.remove(1);
-    		competitors.remove(0);
-    	}
-	}
+        if (competitors.length() == 0){
+            competitors.clear();
+            Final = true;
+        }
+        if (player1 == true){
+            competitors.enQ(currentMatchPlayer1);
+            addRunnerUp(currentMatchPlayer2);
+        }else{
+            competitors.enQ(currentMatchPlayer2);
+            addRunnerUp(currentMatchPlayer1);
+        }
+        System.out.println(competitors.length());
+    }
     
     /** 
      * Get the name of the player/team that finished in position n.  
@@ -85,6 +75,13 @@ public class SingleElimination implements IManager {
      * @return returns the name of the team/player, or null if competition still running or n too large
      */
     public String getPosition(int n) {
-		return (competitors.get(n));
-	}
+        return (competitors.deQ().toString());
+    }
+    public void addRunnerUp(String player){
+        if(competitors.length()==1){
+            competitors.enQ(player);
+        }
+    }
 }
+
+
